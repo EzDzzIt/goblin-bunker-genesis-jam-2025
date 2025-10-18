e5af095045bb72e819a7510d1d9320879edf96ae
nate <n8wlane@gmail.com>
2025-10-17 22:08:18 -0500

    dependencies

--- src/main.c
+++ src/main.c
@@ -1,13 +1,15 @@
 #include <genesis.h>
 #include "resources.h"
 
-int main() {
+int main()
+{
 	VDP_drawText("Hello World!", 4, 4);
 	// Fromt he moon example
 	PAL_setPalette(PAL1, moon.palette->data, DMA);
 	VDP_drawImageEx(BG_A, &moon, TILE_ATTR_FULL(PAL1, 0, 0, 0, 1), 12, 12, 0, CPU);
-	
-	while(TRUE) {
+
+	while (TRUE)
+	{
 		VDP_waitVSync();
 	}
 	return 0;

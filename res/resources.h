cff48407acac20fa026f014750dc7dfa2f247fef
nate <n8wlane@gmail.com>
2025-10-18 14:21:57 -0500

    switching to indexed mode

--- res/resources.h
+++ res/resources.h
@@ -2,5 +2,6 @@
 #define _RES_RESOURCES_H_
 
 extern const Palette palette_1;
+extern const SpriteDefinition player;
 
 #endif // _RES_RESOURCES_H_
--- res/resources.res
+++ res/resources.res
@@ -1,2 +1,2 @@
 PALETTE palette_1 "assets/PAL1.png"
-//SPRITE player "assets/player.png" 2 2 FAST 5
+SPRITE player "assets/player.png" 2 2 FAST
